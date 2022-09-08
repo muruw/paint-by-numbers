6c969be18291a4616926208af2c91140487d0ae3
Marcus Murumäe <marcus.murumae@xolo.io>
2022-09-08 19:14:32 +0300

    fix background color

--- twind.config.ts
+++ twind.config.ts
@@ -1,6 +1,10 @@
 import { Options } from "$fresh/plugins/twind.ts";
+import { apply } from "twind";
 
-  export default {
+export default {
     selfURL: import.meta.url,
-  } as Options;
+    preflight: {
+      body: apply`bg-gradient-to-r from-indigo-800 to-indigo-900`,
+    }
+} as Options;
   
