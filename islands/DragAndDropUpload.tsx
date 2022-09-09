18aa480c0a722eb7cd3241c492418718d60c2da7
Marcus Murumäe <marcus.murumae@xolo.io>
2022-09-09 09:27:43 +0300

    add image upload

--- islands/DragAndDropUpload.tsx
+++ islands/DragAndDropUpload.tsx
@@ -1,8 +1,24 @@
+import { useSignal } from "@preact/signals";
+
+
 export default function DragAndDropUpload() {
+    const uploadedFile = useSignal("");
+
+    const handleFileUpload = (e: any) => {
+        uploadedFile.value = e.target.value;
+    }
+
     return (
-        <div class="flex flex-col h-3/4 justify-center items-center border-dotted border-2 border-gray-400">
-            <p class="text-xl">Drag & Drop</p>
-            <p class="text-sm">or <u class="text-red-600">Browse</u></p>
+        <div class="relative h-3/4 border-dotted border-2 border-gray-400
+                    hover:bg-indigo-800 hover:text-white transition ease-in-out">
+            <input class="absolute w-full h-full cursor-pointer opacity-0" type="file" value={uploadedFile.value} onChange={handleFileUpload}/>
+            <div class="flex flex-col justify-center items-center h-full">
+                <p className="text-xl">Drag & Drop</p>
+                <p className="text-sm">or <u className="text-red-600 hover:text-red-900">Browse</u></p>
+                <span class="success">
+                    {uploadedFile.value}
+                </span>
+            </div>
         </div>
     )
 }
