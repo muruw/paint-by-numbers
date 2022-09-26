e42b32459022815b6af84173bf461b52c783de46
Marcus Murumäe <marcus.murumae@xolo.io>
2022-09-26 22:30:53 +0300

    #9 display image with custom size if not too large inside modal canvas

--- components/upload_image/SvgGeneratorModal.tsx
+++ components/upload_image/SvgGeneratorModal.tsx
@@ -1,5 +1,6 @@
 import ButtonSubmit from "../util/ButtonSubmit.tsx";
 import SvgGeneratorCanvas from "../../islands/SvgGeneratorCanvas.tsx";
+import ButtonClose from '../util/ButtonClose.tsx';
 
 interface ComponentProps {
   closeModal: () => void;
@@ -9,6 +10,12 @@ interface ComponentProps {
 export default function SvgGeneratorModal(
   { closeModal, imageSrc }: ComponentProps,
 ) {
+  const getImage = (imgSrc: string) => {
+    const img = new Image();
+    img.src = imgSrc;
+    return img;
+  };
+
   return (
     <>
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
@@ -17,9 +24,9 @@ export default function SvgGeneratorModal(
           <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
             {/*header*/}
             <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-              <h3 className="text-3xl font-semibold">
+              <h2 className="text-2xl font-semibold">
                 Generate paintable version of your image
-              </h3>
+              </h2>
               <button
                 className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                 onClick={closeModal}
@@ -31,18 +38,12 @@ export default function SvgGeneratorModal(
             </div>
             {/*body*/}
             <div className="relative p-6 flex-auto">
-              <SvgGeneratorCanvas imageSrc={imageSrc} />
+              <SvgGeneratorCanvas image={getImage(imageSrc)} />
             </div>
             {/*footer*/}
             <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-              <button
-                className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={closeModal}
-              >
-                Close
-              </button>
-              <ButtonSubmit label="Generate" onButtonClick={closeModal} />
+              <ButtonSubmit label="Generate" onButtonClick={() => console.log("generate")} />
+              <ButtonClose label="Close" onButtonClick={closeModal} className="mx-2"></ButtonClose>
             </div>
           </div>
         </div>
--- islands/SvgGeneratorCanvas.tsx
+++ islands/SvgGeneratorCanvas.tsx
@@ -1,35 +1,46 @@
 import { useEffect, useRef } from "preact/hooks";
 
 interface SvgGeneratorProps {
-  imageSrc: string;
+  image: any;
 }
 
-const DEFAULT_CANVAS_SIZE = { width: 500, height: 500 };
+const DEFAULT_CANVAS_SIZE = { width: 550, height: 650 };
 
-export default function SvgGeneratorCanvas({ imageSrc }: SvgGeneratorProps) {
+export default function SvgGeneratorCanvas({ image }: SvgGeneratorProps) {
   const canvas = useRef();
+
+  const getWidthAndHeightForCanvas = () => {
+    let h = 0;
+    let w = 0;
+
+    if (image.height > DEFAULT_CANVAS_SIZE.height) {
+      h = DEFAULT_CANVAS_SIZE.height;
+    }
+    if (image.width > DEFAULT_CANVAS_SIZE.width) w = DEFAULT_CANVAS_SIZE.width;
+
+    return { h, w };
+  };
 
   useEffect(() => {
     const context = canvas.current.getContext("2d");
-    const img = new Image();
-    img.src = imageSrc;
-
-    img.onload = () => {
-      context.drawImage(
-        img,
-        0,
-        0,
-        DEFAULT_CANVAS_SIZE.width,
-        DEFAULT_CANVAS_SIZE.height,
-      );
-    };
+    const canvasSize = getWidthAndHeightForCanvas();
+    context.drawImage(
+      image,
+      0,
+      0,
+      canvasSize.w,
+      canvasSize.h,
+    );
   }, []);
+
   return (
-    <canvas
-      ref={canvas}
-      width={DEFAULT_CANVAS_SIZE.width}
-      height={DEFAULT_CANVAS_SIZE.height}
-      className="max-h-full m-2"
-    />
+    <>
+      <canvas
+        ref={canvas}
+        width={getWidthAndHeightForCanvas().w}
+        height={getWidthAndHeightForCanvas().h}
+        className="max-h-full m-2"
+      />
+    </>
   );
 }
